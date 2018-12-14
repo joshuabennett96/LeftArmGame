818f8024c47ac1e0ccea7cca0aaceff9676742c3
Joshua Bennett ジョシュア <jbennett96@live.com>
2018-12-14 11:17:17 +0900

    Create Repo

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(3, rb.velocity.y);
+    }
+}
