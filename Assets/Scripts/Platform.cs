d57c4b7e8a9bd5a13c0bcac6fe21b17c4815821f
Joshua Bennett ジョシュア <jbennett96@live.com>
2018-12-21 12:02:38 +0900

    Spawn Created

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -23,8 +23,12 @@ public class Platform : MonoBehaviour
 		//if (VerticalMove != 0){
 		//	rb.velocity = new Vector2(0, velocity*VerticalMove);
 		//
-		rb.position = new Vector2(rb.position.x,rb.position.y + VerticalMove);
-		Debug.Log(rb.position);
+		if((rb.position.y < 90 && rb.position.y >48)||(rb.position.y >= 90 && VerticalMove < 0)||(rb.position.y <= 48 && VerticalMove >0)){
+			rb.position = new Vector2(rb.position.x,rb.position.y + VerticalMove);
+		}else if(rb.position.y > 45 ){
+			
+		}
+		//Debug.Log(rb.position);
 		//}
 		
 		VerticalMove = 0;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+	public Vector2 spawnPoint = new Vector2 (-154, 73);
+	public float currentX = 0f;
+	public int score;
+	public Text countText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+		countText.Text = "Score: " + score;
     }
 
     void Update()
-    {
+    {	
+		Debug.Log(rb.position.y);
+		currentX = rb.position.x;
         rb.velocity = new Vector2(8, rb.velocity.y);
+		if (currentX >= -52){
+			rb.position = spawnPoint;
+			score = score + 1;
+			countText.Text = "Score: " + score;
+		}
     }
 }
